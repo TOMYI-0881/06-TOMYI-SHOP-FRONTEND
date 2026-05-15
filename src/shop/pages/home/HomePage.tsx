41a167b508605319bb39f24449b8a7b7cee0950a
TOMYI-0881 <gthomasenrique0883@gmail.com>
2026-05-14 19:36:54 -0500

    subida

--- src/shop/pages/home/HomePage.tsx
+++ src/shop/pages/home/HomePage.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@/components/ui/button";
+import CustomPagination from "@/components/custom/CustomPagination";
+import { products } from "@/mocks/Products.mock";
 import { CustomJumbotron } from "@/shop/components/CustomJumbotron";
+import { ProductsGrid } from "@/shop/components/ProductsGrid";
 
 export const HomePage = () => {
   return (
     <>
       <CustomJumbotron title="Todos los Productos" />
+
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={7} />
     </>
   );
 };
